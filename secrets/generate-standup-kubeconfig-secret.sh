7ce13a69b978a3463067bf6e91bb2b04520aa4fe
Yulia Nedyalkova <16097774+yulianedyalkova@users.noreply.github.com>
2020-09-15 12:27:22 +0300

    Tc to cp (#43)
    * Add psp to test runs to use volumes and runAsUser:0
    
    * Fix psp
    
    * Switch from gaia to rfjh2
    
    * Readme
    
    * Default service account in test-workloads can work with pvcs
    
    * Test
    
    * Make psps as minimal as possible
    
    * Switch from in-cluster to using kubeconfig
    
    * Fix volumes of create-cluster task
    
    * secretKeyRef -> secretName
    
    * Add mountPath to volumeMounts
    
    * Fixes
    
    * Document new secrets
    
    * Cleanup task uses config files
    
    * Use master of standup
    
    * Adapt tasks to new standup
    
    * Remove obsolete secret
    
    * Update secrets/standup-endpoints-config-secret.yaml
    
    Co-authored-by: Thomas Fussell <thomas@giantswarm.io>
    
    * Reintroduce script to generate the kubeconfig secret
    
    * add missing backslash
    
    * increase timeout and mount secrets in cleanup
    
    Co-authored-by: Thomas Fussell <thomas@giantswarm.io>

---
+++ secrets/generate-standup-kubeconfig-secret.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+declare -A provider_installations
+provider_installations[aws]=gaia
+provider_installations[azure]=ghost
+provider_installations[kvm]=geckon
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+KUBECONFIG=temp-kubeconfig
+
+for provider in aws azure kvm; do
+    installation=${provider_installations[$provider]}
+    echo "creating $provider kubeconfig for $installation"
+    KUBECONFIG=$KUBECONFIG opsctl create kubeconfig -i $installation --certificate-common-name-prefix test-infra --ttl 30 > /dev/null
+done
+
+KUBECONFIG=$KUBECONFIG kubectl config view --flatten > $KUBECONFIG.flattened
+mv $KUBECONFIG.flattened $KUBECONFIG
+
+kubectl create secret generic standup-kubeconfig -n test-workloads --from-file=kubeconfig=$KUBECONFIG --dry-run=client -o yaml > "$SCRIPT_DIR"/standup-kubeconfig-secret.yaml
+rm $KUBECONFIG
