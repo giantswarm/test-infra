eb02fd660868af73b9d7f35b0b78abc0e6b86ff2
Yulia Nedyalkova <julianedialkova@gmail.com>
2020-09-24 11:23:12 +0300

    Sad attempt to make things less ugly

--- secrets/generate-standup-kubeconfig-secret.sh
+++ secrets/generate-standup-kubeconfig-secret.sh
@@ -7,7 +7,6 @@ provider_installations[kvm]=gorgoth
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 tmp_dir="$script_dir/tmp"
-result_kubeconfig="$tmp_dir/result_kubeconfig"
 service_account="test-infra"
 
 function cleanup() {
@@ -30,7 +29,8 @@ for provider in aws azure kvm; do
     # Get the name of the secret from the CP where the service account token and cert are stored
     secret_name=$(KUBECONFIG=$installation_kubeconfig kubectl -n giantswarm get serviceaccount $service_account -o jsonpath='{.secrets[].name}')
     if [[ -z "$secret_name" ]]; then
-        echo "Could not get the secret name of $service_account service account on $installation. You can create everything you need on the CP by using the files in the control-plane folder."
+        echo "Could not get the secret name of $service_account service account on $installation."
+        echo "You can create everything you need on the CP by using the files in the control-plane folder."
         exit 1
     fi
 
@@ -40,13 +40,31 @@ for provider in aws azure kvm; do
     # Read service account cert from the secret
     KUBECONFIG=$installation_kubeconfig kubectl -n giantswarm get secret/"$secret_name" -o jsonpath='{.data.ca\.crt}'  | base64 --decode > "$tmp_dir/ca_cert"
 
-    # Add data from the service account to the result kubeconfig
+    result_kubeconfig="$tmp_dir/result_kubeconfig"
+    # Add service account data to the kubeconfig
+    (
     touch "$result_kubeconfig"
-    KUBECONFIG="$result_kubeconfig" kubectl config set-cluster "giantswarm-cluster-$installation" --server="$server" --certificate-authority="$tmp_dir/ca_cert" --embed-certs=true
+    export KUBECONFIG="$result_kubeconfig"
 
-    KUBECONFIG="$result_kubeconfig" kubectl config set-credentials "giantswarm-user-$installation" --token="$token"
-    KUBECONFIG="$result_kubeconfig" kubectl config set-context "giantswarm-$installation" --cluster "giantswarm-cluster-$installation" --user "giantswarm-user-$installation"
-    KUBECONFIG="$result_kubeconfig" kubectl config use-context "giantswarm-$installation"
+    # Add data from the service account to the result kubeconfig
+    kubectl config set-cluster "giantswarm-cluster-$installation" \
+        --server="$server" \
+        --certificate-authority="$tmp_dir/ca_cert" \
+        --embed-certs=true
+
+   kubectl config set-credentials "giantswarm-user-$installation" \
+        --token="$token"
+
+   kubectl config set-context "giantswarm-$installation" \
+        --cluster "giantswarm-cluster-$installation" \
+        --user "giantswarm-user-$installation"
+
+   kubectl config use-context "giantswarm-$installation"
+   )
 done
 
-kubectl create secret generic standup-kubeconfig -n test-workloads --from-file=kubeconfig="$result_kubeconfig" --dry-run=client -o yaml > "$script_dir/standup-kubeconfig-secret.yaml"
+kubectl create secret generic standup-kubeconfig \
+    -n test-workloads \
+    --from-file=kubeconfig="$result_kubeconfig" \
+    --dry-run=client -o yaml \
+    > "$script_dir/standup-kubeconfig-secret.yaml"
